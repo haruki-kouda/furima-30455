92a157da57ec671847ca6e957468308b7c303e0c
haruki-kouda <71618264+haruki-kouda@users.noreply.github.com>
2020-10-15 19:57:52 +0900

    ログイン機能の設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
     validates :nickname,presence: true
-    validates :encrypted_password,presence: true  #, { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+    validates :encrypted_password,presence: true , format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
     validates :first_name,presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, }
-    validates :last_name,presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, }
+    validates :family_name,presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, }
     validates :first_name_kana,presence: true, format: { with: /\A[ァ-ン]+\z/, }
-    validates :last_name_kana,presence: true, format: { with: /\A[ァ-ン]+\z/, }
+    validates :family_name_kana,presence: true, format: { with: /\A[ァ-ン]+\z/, }
     validates :birth_day,presence: true
 
   has_many :items
