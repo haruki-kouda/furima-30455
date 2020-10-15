5db78a1e92dc25484d0b9ba341abb4bb673e5708
haruki-kouda <71618264+haruki-kouda@users.noreply.github.com>
2020-10-15 09:24:13 +0900

    　userモデルの作成

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+    validates :nickname,presence: true
+    validates :encrypted_password,presence: true  #, { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+    validates :first_name,presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, }
+    validates :last_name,presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, }
+    validates :first_name_kana,presence: true, format: { with: /\A[ァ-ン]+\z/, }
+    validates :last_name_kana,presence: true, format: { with: /\A[ァ-ン]+\z/, }
+    validates :birth_day,presence: true
+
+  has_many :items
+  has_many :item_purchases
+end
