d6e9c31fa9caf600f3e1c805f4dbd2027923f136
haruki-kouda <71618264+haruki-kouda@users.noreply.github.com>
2020-10-16 22:45:17 +0900

    商品バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,19 @@
 class Item < ApplicationRecord
-  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :goods_condition, :postage_type, :delivery_area, :preparation_day
 
+  with_options presence: true do
+    validates :name
+    validates :category 
+    validates :goods_condition
+    validates :postage_type
+    validates :delivery_area
+    validates :preparation_day 
+    validates :price
+    validates :user
+  end
+
+  validates :genre_id, :goods_condition_id, :postage_type_id, :delivery_area, :preparation_day, numericality: { other_than: 1 }
+  
+  has_one_attached :image
 end
