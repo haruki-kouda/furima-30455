7103b5db5e97e3777c15ab59fea0050c14e8df4d
haruki-kouda <71618264+haruki-kouda@users.noreply.github.com>
2020-10-16 12:38:06 +0900

    ログイン時のエラーメッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :password, presence: true, format: { with: /\A[a-z0-9]+\z/i }
   validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :encrypted_password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :password, presence: true, format: { with: /\A[a-z0-9]+\z/i }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
   validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
