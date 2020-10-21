4e6535b7c596920fcbf92ab2eb03544b3509d206
haruki-kouda <71618264+haruki-kouda@users.noreply.github.com>
2020-10-21 21:00:52 +0900

    各ログイン時によるページ遷移の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, only: :edit
   before_action :authenticate_user!, except: [:index, :show]
   def index
     @item = Item.includes(:user).order('created_at DESC')
@@ -24,18 +25,24 @@ class ItemsController < ApplicationController
   def edit
     @item = Item.find(params[:id])
   end
-  
+
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to  item_path
+      redirect_to item_path
     else
       render :edit
     end
   end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :goods_condition_id, :postage_type_id, :delivery_area_id, :preparation_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    redirect_to action: :index if user_signed_in? && current_user.id != @item.user_id
   end
 end
