f7c90bd64ff4ce67d7dc2a8f6a3bf0abb6618fea
haruki-kouda <71618264+haruki-kouda@users.noreply.github.com>
2020-10-29 22:05:19 +0900

    購入機能の実装

---
+++ app/controllers/item_purchases_controller.rb
@@ -0,0 +1,48 @@
+class ItemPurchasesController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+  before_action :move_to_order
+  before_action :authenticate_user!
+  before_action :move_to_item
+
+  def index
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:post_code, :prefecture_code_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: Item.find(params[:item_id]).price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_item
+    redirect_to root_path if current_user.id == @item.user_id
+  end
+
+  def move_to_order
+    redirect_to root_path unless @item.item_purchase.nil?
+  end
+end
--- app/models/sending_destination.rb
+++ app/models/sending_destination.rb
@@ -1,17 +1,6 @@
 class SendingDestination < ApplicationRecord
   belongs_to :item_purchase
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :prefecture_code
-
-  with_options presence: true do
-    validates :post_code, format: { with: /\A[0-9]+\z/ },
-    validates :prefecture_code
-    validates :city
-    validates :house_number
-    validates :phone_number, format: { with: /\A[0-9]+\z/ },
-  end
-
-  varidates :building_name
-  varidates :prefecture_code_id, numericality: { other_than: 1 } 
 end
+
+# extend ActiveHash::Associations::ActiveRecordExtensions
+# belongs_to_active_hash :prefecture_code
